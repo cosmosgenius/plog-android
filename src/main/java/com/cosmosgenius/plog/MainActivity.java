5e57208f18da79ae4dc8b07db7158f6a58e4d299
Sharat M R <cosmosgenius@gmail.com>
2013-11-01 03:12:14 +0530

    added a gradle project

---
+++ src/main/java/com/cosmosgenius/plog/MainActivity.java
@@ -0,0 +1,22 @@
+package com.cosmosgenius.plog;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("Hello world!");
+    }
+
+}
