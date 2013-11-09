e2fb43d05ab2181d7c6316271f92d1f325265119
Sharat M R <cosmosgenius@gmail.com>
2013-11-10 01:58:12 +0530

    added comments

--- src/main/java/com/cosmosgenius/plog/MainActivity.java
+++ src/main/java/com/cosmosgenius/plog/MainActivity.java
@@ -5,37 +5,40 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-
 public class MainActivity extends Activity {
-    ImageButton btn_log;
-    EditText log_text;
-    LogListAdapter logListAdapter;
+    ImageButton btn_log;            // send Button resource
+    EditText log_text;              // text input box
+    LogListAdapter logListAdapter;  // The adapter attached to the listview
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        // setting the objects
         btn_log = (ImageButton) findViewById(R.id.btn_log);
         log_text = (EditText) findViewById(R.id.input_log);
-        enableBtn_log(false);
         ListView log_list = (ListView) findViewById(R.id.log_list);
+
+        // By default disabling the Send button
+        // TODO : Create a custom button with custom enable and disable option
+        enableBtn_log(false);
+
+        //attaching the Adapter to the list view
         logListAdapter = new LogListAdapter(getApplicationContext());
-/*        String [] l = {"Sharat","Praseetha"};
-        for(int i = 0; i< l.length ; i++){
-            logListAdapter.add(l[i]);
-        }
-        logListAdapter.add("Logs");*/
         log_list.setAdapter(logListAdapter);
     }
 
     @Override
     public void onStart() {
         super.onStart();
+
+        //attaching a text change listener to the edit text to find whether to
+        //enable the send button or not
         log_text.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
@@ -55,14 +58,17 @@ public class MainActivity extends Activity {
         });
     }
 
+    //send button click event handler
     public void btn_log_click(View view){
-        log_text = (EditText) findViewById(R.id.input_log);
+        //getting the text for edit text and added to the list adapter
         if((log_text.getText() != null) && (log_text.getText().length() > 0)){
             logListAdapter.add(log_text.getText().toString());
         }
+        //clearing the text after the text has been added to the list adapter
         log_text.setText("");
     }
 
+    //Function to whether enable or disable the function
     void enableBtn_log(boolean value){
         btn_log.setEnabled(value);
         if(value){
