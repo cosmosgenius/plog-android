10ed1f269d85d8ad14ed3a2d92dc01c929c6e4ed
Sharat M R <cosmosgenius@gmail.com>
2014-04-21 01:59:17 +0530

    got components

--- src/main/java/com/cosmosgenius/plog/activity/LogListActivity.java
+++ src/main/java/com/cosmosgenius/plog/activity/LogListActivity.java
@@ -2,15 +2,27 @@ package com.cosmosgenius.plog.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.cosmosgenius.plog.R;
 
 public class LogListActivity extends Activity{
 
+    private ImageButton add_log;
+    private EditText input_log;
+    private ListView log_list;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.id.log_list_activity);
+        setContentView(R.layout.log_list_activity);
+
+        add_log     = (ImageButton) findViewById(R.id.add_log);
+        input_log   = (EditText) findViewById(R.id.input_log);
+        log_list    = (ListView) findViewById(R.id.log_list);
+
     }
 
     @Override
