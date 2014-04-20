44dbf048e4cb20c63647135f9a460404b2d34aeb
Sharat M R <cosmosgenius@gmail.com>
2014-04-20 22:34:47 +0530

    added Log bean with bean package

---
+++ src/main/java/com/cosmosgenius/plog/bean/Log.java
@@ -0,0 +1,22 @@
+package com.cosmosgenius.plog.bean;
+
+public class Log {
+    private String plog;
+    private String _id;
+
+    public String getPlog() {
+        return plog;
+    }
+
+    public void setPlog(String plog) {
+        this.plog = plog;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+}
---
+++ src/main/java/com/cosmosgenius/plog/service/LogService.java
@@ -0,0 +1,7 @@
+package com.cosmosgenius.plog.service;
+
+/**
+ * Created by Sharat on 20-04-2014.
+ */
+public interface LogService {
+}
