e3e465f0515e529959d8cc0981be33494116eb92
Sharat M R <cosmosgenius@gmail.com>
2014-04-21 01:25:25 +0530

    added Log List Activity and log item

---
+++ src/main/java/com/cosmosgenius/plog/activity/LogListActivity.java
@@ -0,0 +1,17 @@
+package com.cosmosgenius.plog.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogListActivity extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+}
