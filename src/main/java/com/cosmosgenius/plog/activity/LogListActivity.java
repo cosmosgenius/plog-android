24a4cff6aaecceb4666bb7db97f3931f5ee17f24
Sharat M R <cosmosgenius@gmail.com>
2014-04-21 01:42:22 +0530

    update log list activity and added log_list_activity.xml

--- src/main/java/com/cosmosgenius/plog/activity/LogListActivity.java
+++ src/main/java/com/cosmosgenius/plog/activity/LogListActivity.java
@@ -3,11 +3,14 @@ package com.cosmosgenius.plog.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.cosmosgenius.plog.R;
+
 public class LogListActivity extends Activity{
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.id.log_list_activity);
     }
 
     @Override
