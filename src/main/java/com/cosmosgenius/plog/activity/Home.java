99288424f782e2895227d972beb58ccd2aa613bb
Sharat M R <cosmosgenius@gmail.com>
2015-05-17 19:20:49 +0530

    added a launch activity

--- src/main/java/com/cosmosgenius/plog/activity/Home.java
+++ src/main/java/com/cosmosgenius/plog/activity/Home.java
@@ -1,9 +1,13 @@
 package com.cosmosgenius.plog.activity;
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Sharat on 16-05-2015.
  */
 public class Home extends Activity{
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
