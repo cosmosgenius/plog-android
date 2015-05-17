04ac16fb248878ca3ec5a2c3cd0574555e9d0e73
Sharat M R <cosmosgenius@gmail.com>
2015-05-18 02:58:18 +0530

    added home layout

--- src/main/java/com/cosmosgenius/plog/activity/HomeActivity.java
+++ src/main/java/com/cosmosgenius/plog/activity/HomeActivity.java
@@ -2,12 +2,15 @@ package com.cosmosgenius.plog.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.cosmosgenius.plog.R;
+
 /**
  * Created by Sharat on 16-05-2015.
  */
-public class Home extends Activity{
+public class HomeActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
     }
 }
